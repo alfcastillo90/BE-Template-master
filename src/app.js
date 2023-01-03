6fce918c15363045bee5fba759e61b1c9caf31ff
Carlos Castillo <cacr1990@gmail.com>
2023-01-03 20:58:26 -0300

    ADD:  Pay for a job post method.

--- src/app.js
+++ src/app.js
@@ -99,6 +99,68 @@ app.get('/jobs/unpaid',getProfile ,async (req, res) =>{
   } catch (error) {
       res.status(500).json({ message: error.message })
   }
+});
+
+/**
+ * @params job_id
+ * @header profile_id
+ * @returns result true
+ */
+
+app.post('/jobs/:job_id/pay',getProfile ,async (req, res) =>{
+  const sequelize = req.app.get('sequelize')
+  const { Job, Contract, Profile } = req.app.get('models');
+  const profileBalance = req.profile.balance;
+  const profileId = req.profile.id;
+  const jobId = req.params.job_id;
+
+  try {
+    if (profile.type !== 'client') {
+      res.status(422).json({
+        message: 'This operation is just available for clients'
+      })
+    }
+
+    const job = await Job.findOne({
+        include: [{
+            model: Contract,
+            required: true,
+            where: { ClientId: profileId }
+        }],
+        where: {id: jobId}
+    });
+
+    if(!job) {
+      return res.status(422).json({
+          message: `There are not any jobs for client with profile id ${profileId}`
+      })
+    }
+    const jobPrice = job.price;
+
+    if(profileBalance < jobPrice) {
+      return res.status(422).json({
+        message: `Profile balance amount must be major than job price`
+      })
+    }
+  
+    const transaction = await sequelize.transaction();
+    
+    await Promise.all([
+      Job.update({paid: true, paymentDate: new Date()}, {where: {id: jobId}, transaction}),
+      Profile.increment('balance', {by: job.price, where: { id: job.Contract.ContractorId}, transaction}),
+      Profile.decrement('balance', {by: job.price, where: { id: job.Contract.ClientId}, transaction})
+    ]);
+    
+    await transaction.commit()
+      
+    res.status(200).json({
+      result: true
+    })
+  } catch (error) {
+      if (transaction) await transaction.rollback()
+      return res.status(500).end()
+  }
+  
 })
 
 
