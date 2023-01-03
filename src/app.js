509c320b8246cf13f125f092620473a572618f60
Carlos Castillo <cacr1990@gmail.com>
2023-01-03 20:13:45 -0300

    FIX:  contracts by id

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model');
+const { getProfile } = require('./middleware/getProfile');
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
+const { Op } = require('sequelize');
 
 /**
- * FIX ME!
+ * @params contractId
+ * @header profile_id
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get('/contracts/:id', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models');
+  const { id } = req.params;
+  const profileId = req.profile.id;
+
+  const contract = await Contract.findOne({ 
+    where: { 
+        id,
+        [Op.or]: [
+            { ClientId: profileId }, 
+            { ContractorId: profileId }
+        ]
+    } 
+  });
+  
+  if (!contract) { 
+    return res.status(404).json({message: 'Not found'})
+  } else {
+    res.json(contract);
+  }
+});
 module.exports = app;
